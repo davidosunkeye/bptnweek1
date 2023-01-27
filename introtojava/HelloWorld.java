bf2a8f9674c0f65ce2bc49ad13e803802a7b477a
Oladapo Osunkeye <davidosunkeye@yahoo.com>
2023-01-27 05:48:54 -0500

    Modified the HelloWorld File

--- introtojava/HelloWorld.java
+++ introtojava/HelloWorld.java
@@ -1,37 +1,10 @@
 package introtojava;
-import java.util.Scanner; // import the Scanner class 
-
 public class HelloWorld {
-	 private static String favCity; 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
+	    System.out.println("Oladapo David Osunkeye");
 
-	    System.out.println("Enter name, favorite city, age,  and salary:");
-
-	    // String input
-	    String name = scanner.nextLine();
-
-	    // string input initializing variable that previously declared
-	    favCity = scanner.nextLine();
-
-	    // Numerical input - integer
-	    int age = scanner.nextInt();
-
-	    // numeric input - double
-	    double salary = scanner.nextDouble();
-
-	    // Output input by user
-	    System.out.println("Name: " + name); 
-	    System.out.println("Favorite city: " + getFavCity());
-	    System.out.println("Age: " + age); 
-	    System.out.println("Salary: " + salary);
-
-	    scanner.close();
-	  }
-	 private static String getFavCity(){
-	      return favCity;
 	  }
 	}
 
